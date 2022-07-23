7ec60b92c8807d77402fe45b56aececf9f5ceb64
zhangran19 <zhangran19@tal.com>
2022-07-23 11:08:01 +0800

    2

--- server.go
+++ server.go
@@ -3,20 +3,51 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
-	Ip   string
-	Port string
+	Ip        string
+	Port      string
+	Message   chan string
+	OnlineMap map[string]*User
+	mplock    sync.RWMutex
 }
 
 func NewServer(ip string, port string) *Server {
-	server := &Server{ip, port}
+	server := &Server{ip, port, make(chan string), make(map[string]*User), sync.RWMutex{}}
 	return server
+}
+
+func (this *Server) Broadcast(user *User, msg string) {
+	message := "[" + user.Address + "]" + user.Name + ":" + msg
+	this.Message <- message
 }
 
 func (This *Server) handle(conn net.Conn) {
 	fmt.Println("链接建立成功")
+	user := NewUser(conn)
+
+	//添加到在线用户map
+	This.mplock.Lock()
+	This.OnlineMap[user.Name] = user
+	This.mplock.Unlock()
+
+	//通过带缓存chaneel用户上线
+	This.Broadcast(user, "用户上线了")
+}
+
+func (this *Server) xx() {
+	for {
+		msg := <-this.Message
+		this.mplock.Lock()
+
+		for _, user := range this.OnlineMap {
+			user.C <- msg
+		}
+
+		this.mplock.Unlock()
+	}
 }
 
 func (this *Server) Start() {
